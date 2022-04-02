31fb21a650ad440ce0057c70ea3cf0e86dc8e5aa
jackiestchen <jackiestchen@gmail.com>
2022-04-01 23:43:56 -0600

    Use calculation methods in db

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+total_revenue = Store.sum("annual_revenue")
+average_revenue = Store.average("annual_revenue")
+
+puts "Total Revenue: #{total_revenue}"
+puts "Average Revenue: #{average_revenue}"
+
 # Your code goes here ...
