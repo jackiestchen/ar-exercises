b890e01e0efc7162e2b18ca5f6ff3ec277b3f915
jackiestchen <jackiestchen@gmail.com>
2022-04-03 00:12:17 -0600

    Add tests for validations

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,11 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
+puts "Please input a store name"
+store_name = $stdin.gets.chomp.to_s
+puts "Attemptting to create a store with #{store_name}"
+new_store = Store.create(name: store_name)
+pp new_store.errors.full_messages
+
+
 # Your code goes here ...
